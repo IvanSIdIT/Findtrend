8ff8c4a281fd2dd2660d42079df8c5626d9831fe
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2023-03-05 14:53:27 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
 function isChecked0() {
   if (document.getElementById('check').checked) {
     document.getElementById('price0').textContent = `${
-      +document.getElementById('price0').innerHTML * 12
+      +document.getElementById('price0').innerHTML * 11
     } `;
     document.getElementById('dur0').textContent = '/yearly';
   } else {
     document.getElementById('price0').textContent = `${
-      document.getElementById('price0').innerHTML / 12
+      document.getElementById('price0').innerHTML / 11
     }`;
     document.getElementById('dur0').textContent = '/monthly';
   }
@@ -15,12 +15,12 @@ function isChecked0() {
 function isChecked1() {
   if (document.getElementById('check').checked) {
     document.getElementById('price1').textContent = `${
-      +document.getElementById('price1').innerHTML * 12
+      +document.getElementById('price1').innerHTML * 11
     } `;
     document.getElementById('dur1').textContent = '/yearly';
   } else {
     document.getElementById('price1').textContent = `${
-      document.getElementById('price1').innerHTML / 12
+      document.getElementById('price1').innerHTML / 11
     }`;
     document.getElementById('dur1').textContent = '/monthly';
   }
@@ -29,12 +29,12 @@ function isChecked1() {
 function isChecked2() {
   if (document.getElementById('check').checked) {
     document.getElementById('price2').textContent = `${
-      +document.getElementById('price2').innerHTML * 12
+      +document.getElementById('price2').innerHTML * 11
     } `;
     document.getElementById('dur2').textContent = '/yearly';
   } else {
     document.getElementById('price2').textContent = `${
-      document.getElementById('price2').innerHTML / 12
+      document.getElementById('price2').innerHTML / 11
     }`;
     document.getElementById('dur2').textContent = '/monthly';
   }
@@ -46,16 +46,3 @@ function isChecked() {
   isChecked2();
   document.getElementById('test').classList.toggle('active');
 }
-
-// function isChecked() {
-//   document.getElementById('test').classList.toggle('active');
-//   if (document.getElementById('check').checked) {
-//     document.querySelectorAll('.price').textContent = `${
-//       +document.querySelectorAll('.price').innerHTML * 12
-//     } `;
-//   } else {
-//     document.querySelectorAll('.price').textContent = `${
-//       document.querySelectorAll('.price').innerHTML / 12
-//     }`;
-//   }
-// }
